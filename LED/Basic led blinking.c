eb8a580654531ce2bb1fa09746590520bd420bd8
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-02 22:26:33 +0530

    Basic led blinking.c

---
+++ LED/Basic led blinking.c
@@ -0,0 +1,23 @@
+//Embedded C program for basic LED blinking using "ARM based LPC2129 MICROCONTROLLER".
+#include<LPC21XX.H>
+void delay(unsigned int);
+int main()
+{
+IODIR0=0X1;
+while(1)
+{
+IOCLR0=0X1;
+delay(2);
+IOSET0=0X1;
+delay(2);
+}
+}
+
+void delay(unsigned int t)
+{
+T0PR=15000000-1; 
+T0TCR=0X1;
+while(T0TC<t);
+T0TCR=0X3;
+T0TCR=0X0;
+}
