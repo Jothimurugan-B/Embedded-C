f4a4cdbae636341635d2d854f99829805086674c
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-03 09:25:59 +0530

    LED divergence pattern blinking.c

---
+++ LED/LED divergence pattern blinking.c
@@ -0,0 +1,30 @@
+//Embedded C program for blinking LEDs in divergence pattern using "ARM based LPC2129 MICROCONTROLLER".
+#include<LPC21XX.H>
+void delay(int);
+int main()
+{	
+int i,j;
+   IODIR0=0XFF; // Set P0.0–P0.7 as output
+   IOSET0=0XFF; // Turn off all LEDs initially (active low LEDs)
+  while(1)
+{
+  for(i=3,j=4; i>=0 && j<8; i--,j++)
+  {
+   
+	IOCLR0=1<<i | 1<<j; //Turn on selected 2 LEDs
+	delay(500);
+	IOSET0=1<<i | 1<<j; //Turn off selected 2 LEDs 
+	delay(500);  //providing 500 milliseconds delay
+	}
+}
+}
+void delay(int t)
+{
+  T0PR=15000-1;
+  T0TCR=0X1;
+  while(T0TC<t);
+  T0TCR=0X3;
+  T0TCR=0X0;
+ } 
+
+
