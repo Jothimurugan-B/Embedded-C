8dafcc8fb38ce0f7f3f5634ec4d8555215dc938a
Jothimurugan B <b.jothimurugan2004@gmail.com>
2025-08-03 10:50:03 +0530

    Create Display string in LCD.c
    Embedded C program to display a string in LCD using ARM based LPC2129 microcontroller and 16*2 alphanumeric LCD.

---
+++ LCD/Display string in LCD.c
@@ -0,0 +1,57 @@
+//Embedded C program to display a string in LCD
+#include<LPC21XX.H>
+#include"delay.h" //including delay code as header
+
+#define LCD 0XFF
+#define RS 1<<8
+#define E 1<<9
+
+void init();
+void command(unsigned char);
+void string(unsigned char*);
+void data(unsigned char);
+int main()
+{
+   init();
+   command(0X80); //display in the address (0X80)
+   string("B.Jothimurugan"); 
+  
+ }
+ void init()
+ {
+   IODIR0=0X3FF;
+   command(0X1);
+   command(0X2);
+   command(0X0C);
+   command(0X38);
+  }
+
+  void command(unsigned char cmd)
+  {
+  IOCLR0=LCD;
+	IOSET0=cmd;
+	IOCLR0=RS;
+	IOSET0=E;
+	delay_millisec(2); //delay provided for internal operations of LCD to be done
+	IOCLR0=E;
+  }
+
+  void string(unsigned char* s)
+  {
+    int i;
+    for(i=0;s[i];i++)
+  	data(s[i]);
+  }
+
+  void data(unsigned char ch)
+  {
+  IOCLR0=LCD;
+	IOSET0=ch;
+	IOSET0=RS;
+	IOSET0=E;
+	delay_millisec(2);
+	IOCLR0=E;
+  }
+
+
+   
